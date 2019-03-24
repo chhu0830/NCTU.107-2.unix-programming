25a68c5544bdec1666f5d389c5593428e92f8d3c
Chia-Hsuan Hu <chhu0830@gmail.com>
2019-03-24 15:21:15 +0800

    Read cmdline

--- proj1/src/main.c
+++ proj1/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <ctype.h>
@@ -17,22 +18,69 @@ struct CONNECTION {
     unsigned int inode;
 };
 
+struct PROCINFO {
+    unsigned short int pid;
+    unsigned int inode;
+    char cmdline[512];
+};
 
-unsigned int read_connection(struct CONNECTION *list, char *filename, char type);
+
+unsigned int read_connection(struct CONNECTION *list, char *filename, int type);
+unsigned int read_fd(struct PROCINFO *info);
+// TODO: Try not to pass entire `info` into read_cmdline
+void read_cmdline(char *pid, struct PROCINFO *info);
 char is_str_digit(char *str);
 
 
 int main() {
+    struct PROCINFO info[1024];
+    unsigned int nconn[4], ninfo = 0;
+    struct CONNECTION connections[4][1024];
+
+    ninfo = read_fd(info);
+
+    nconn[0] = read_connection(connections[0], "/proc/net/tcp", 0);
+    nconn[1] = read_connection(connections[1], "/proc/net/udp", 1);
+
+    return 0;
+}
+
+unsigned int read_connection(struct CONNECTION *list, char *filename, int type) {
+    FILE *file = fopen(filename, "r");
+    unsigned int idx, dummy, local_ip, local_port, rmt_ip, rmt_port, inode;
+    char str[256];
+    
+    fgets(str, sizeof(str), file);
+
+    for (idx = 0; fscanf(file, "%u:", &dummy) != EOF; idx++) {
+        fscanf(file, "%x:%x %x:%x", &local_ip, &local_port, &rmt_ip, &rmt_port);
+
+        for (int i = 0; i < 6; i++) {
+            fscanf(file, "%s", str);
+        }
+
+        fscanf(file, "%u", &inode);
+        
+        for (int i = 0, n = (type & 1 ? 3 : 7); i < n; i++) {
+            fscanf(file, "%s", str);
+        }
+
+        inet_ntop(AF_INET, &local_ip, list[idx].local_ip, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &rmt_ip, list[idx].rmt_ip, INET_ADDRSTRLEN);
+        list[idx].local_port = local_port;
+        list[idx].rmt_port = rmt_port;
+        list[idx].inode = inode;
+    }
+    
+    return idx;
+}
+
+unsigned int read_fd(struct PROCINFO *info) {
     char fdpath[512], lnpath[1024], tgpath[256];
     DIR *proc, *fd;
     struct dirent *pid, *fdnum;
+    unsigned int idx = 0;
 
-    struct CONNECTION tcp[1024], udp[1024];
-    memset(tcp, 0, sizeof(tcp));
-    memset(udp, 0, sizeof(udp));
-    read_connection(tcp, "/proc/net/tcp", 0);
-    read_connection(udp, "/proc/net/udp", 1);
-    
     proc = opendir("/proc");
     
     while ((pid = readdir(proc)) != NULL) {
@@ -60,45 +108,33 @@ int main() {
             memset(tgpath, 0, sizeof(tgpath));
             readlink(lnpath, tgpath, sizeof(tgpath));
 
-            // Get inode if the fd is socket
-            int inode;
-            if (sscanf(tgpath, "socket:[%d]", &inode)) {
+            // Get info if the fd is socket
+            unsigned int inode;
+            if (sscanf(tgpath, "socket:[%u]", &inode)) {
+                info[idx].pid = atoi(pid->d_name);
+                info[idx].inode = inode;
+                read_cmdline(pid->d_name, &info[idx]);
             }
-
         }
     }
-    
-    return 0;
+
+    return idx;
 }
 
-unsigned int read_connection(struct CONNECTION *list, char *filename, char type) {
-    FILE *file = fopen(filename, "r");
-    unsigned int c, dummy, local_ip, local_port, rmt_ip, rmt_port, inode;
-    char str[256];
+void read_cmdline(char *pid, struct PROCINFO *info) {
+    char cmdpath[256];
+    sprintf(cmdpath, "/proc/%s/cmdline", pid);
+
+    FILE *cmdfile = fopen(cmdpath, "r");
     
-    fgets(str, sizeof(str), file);
+    int n = fread(info->cmdline, 1, sizeof(info->cmdline), cmdfile);
+    info->cmdline[n] = '\0';
 
-    for (c = 0; fscanf(file, "%u:", &dummy) != EOF; c++) {
-        fscanf(file, "%x:%x %x:%x", &local_ip, &local_port, &rmt_ip, &rmt_port);
-
-        for (int i = 0; i < 6; i++) {
-            fscanf(file, "%s", str);
+    while (--n) {
+        if (info->cmdline[n] == '\0' || isspace(info->cmdline[n])) {
+            info->cmdline[n] = ' ';
         }
-
-        fscanf(file, "%u", &inode);
-        
-        for (int i = 0, n = (type & 1 ? 3 : 7); i < n; i++) {
-            fscanf(file, "%s", str);
-        }
-
-        inet_ntop(AF_INET, &local_ip, list[c].local_ip, INET_ADDRSTRLEN);
-        inet_ntop(AF_INET, &rmt_ip, list[c].rmt_ip, INET_ADDRSTRLEN);
-        list[c].local_port = local_port;
-        list[c].rmt_port = rmt_port;
-        list[c].inode = inode;
     }
-    
-    return c;
 }
 
 char is_str_digit(char *str) {
