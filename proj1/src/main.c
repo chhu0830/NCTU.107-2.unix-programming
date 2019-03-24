e13012263c3660ac1b48e7f08566b214d012a5b1
Chia-Hsuan Hu <chhu0830@gmail.com>
2019-03-24 11:47:35 +0800

    Read tcp and udp connections

--- proj1/src/main.c
+++ proj1/src/main.c
@@ -5,24 +5,33 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 
 #define false 0
 #define true 1
 
-short int is_str_digit(char *str) {
-    for (int i = 0; str[i]; i++) {
-        if (!isdigit(str[i])) {
-            return false;
-        }
-    }
 
-    return true;
-}
+struct CONNECTION {
+    char local_ip[32], rmt_ip[32];
+    unsigned short int local_port, rmt_port;
+    unsigned int inode;
+};
+
+
+unsigned int read_connection(struct CONNECTION *list, char *filename, char type);
+char is_str_digit(char *str);
+
 
 int main() {
     char fdpath[512], lnpath[1024], tgpath[256];
     DIR *proc, *fd;
     struct dirent *pid, *fdnum;
+
+    struct CONNECTION tcp[1024], udp[1024];
+    memset(tcp, 0, sizeof(tcp));
+    memset(udp, 0, sizeof(udp));
+    read_connection(tcp, "/proc/net/tcp", 0);
+    read_connection(udp, "/proc/net/udp", 1);
     
     proc = opendir("/proc");
     
@@ -54,11 +63,50 @@ int main() {
             // Get inode if the fd is socket
             int inode;
             if (sscanf(tgpath, "socket:[%d]", &inode)) {
-                printf("%s %s %d\n", lnpath, tgpath, inode);
             }
 
         }
     }
     
     return 0;
+}
+
+unsigned int read_connection(struct CONNECTION *list, char *filename, char type) {
+    FILE *file = fopen(filename, "r");
+    unsigned int c, dummy, local_ip, local_port, rmt_ip, rmt_port, inode;
+    char str[256];
+    
+    fgets(str, sizeof(str), file);
+
+    for (c = 0; fscanf(file, "%u:", &dummy) != EOF; c++) {
+        fscanf(file, "%x:%x %x:%x", &local_ip, &local_port, &rmt_ip, &rmt_port);
+
+        for (int i = 0; i < 6; i++) {
+            fscanf(file, "%s", str);
+        }
+
+        fscanf(file, "%u", &inode);
+        
+        for (int i = 0, n = (type & 1 ? 3 : 7); i < n; i++) {
+            fscanf(file, "%s", str);
+        }
+
+        inet_ntop(AF_INET, &local_ip, list[c].local_ip, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &rmt_ip, list[c].rmt_ip, INET_ADDRSTRLEN);
+        list[c].local_port = local_port;
+        list[c].rmt_port = rmt_port;
+        list[c].inode = inode;
+    }
+    
+    return c;
+}
+
+char is_str_digit(char *str) {
+    for (int i = 0; str[i]; i++) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
