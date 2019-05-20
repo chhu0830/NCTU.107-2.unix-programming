ac0db80c74bfc03eee77fbb2078764da8cda3dd3
Chia-Hsuan Hu <chhu0830@gmail.com>
2019-05-20 22:34:52 +0800

    Add inclass example

--- proj3/src/libmini.asm
+++ proj3/src/libmini.asm
@@ -1,37 +1,115 @@
 
 %macro gensys 2
-    global sys_%2:function
+	global sys_%2:function
 sys_%2:
-    push r10
-    mov r10, rcx
-    mov rax, %1
-    syscall
-    pop r10
-    ret
+	push	r10
+	mov	r10, rcx
+	mov	rax, %1
+	syscall
+	pop	r10
+	ret
 %endmacro
 
-extern errno
+; RDI, RSI, RDX, RCX, R8, R9
 
-    section .data
+extern	errno
 
-    section .text
+	section .data
 
-    gensys   1, write
+	section .text
+
+	gensys   0, read
+	gensys   1, write
+	gensys   2, open
+	gensys   3, close
+	gensys   9, mmap
+	gensys  10, mprotect
+	gensys  11, munmap
     gensys  13, rt_sigaction
     gensys  14, rt_sigprocmask
-    gensys  34, pause
-    gensys  35, nanosleep
+	gensys  22, pipe
+	gensys  32, dup
+	gensys  33, dup2
+	gensys  34, pause
+	gensys  35, nanosleep
     gensys  37, alarm
-    gensys  60, exit
+	gensys  57, fork
+	gensys  60, exit
+	gensys  79, getcwd
+	gensys  80, chdir
+	gensys  82, rename
+	gensys  83, mkdir
+	gensys  84, rmdir
+	gensys  85, creat
+	gensys  86, link
+	gensys  88, unlink
+	gensys  89, readlink
+	gensys  90, chmod
+	gensys  92, chown
+	gensys  95, umask
+	gensys  96, gettimeofday
+	gensys 102, getuid
+	gensys 104, getgid
+	gensys 105, setuid
+	gensys 106, setgid
+	gensys 107, geteuid
+	gensys 108, getegid
     gensys 127, rt_sigpending
 
+	global open:function
+open:
+	call	sys_open
+	cmp	rax, 0
+	jge	open_success	; no error :)
+open_error:
+	neg	rax
+%ifdef NASM
+	mov	rdi, [rel errno wrt ..gotpc]
+%else
+	mov	rdi, [rel errno wrt ..gotpcrel]
+%endif
+	mov	[rdi], rax	; errno = -rax
+	mov	rax, -1
+	jmp	open_quit
+open_success:
+%ifdef NASM
+	mov	rdi, [rel errno wrt ..gotpc]
+%else
+	mov	rdi, [rel errno wrt ..gotpcrel]
+%endif
+	mov	QWORD [rdi], 0	; errno = 0
+open_quit:
+	ret
+
+	global sleep:function
+sleep:
+	sub	rsp, 32		; allocate timespec * 2
+	mov	[rsp], rdi		; req.tv_sec
+	mov	QWORD [rsp+8], 0	; req.tv_nsec
+	mov	rdi, rsp	; rdi = req @ rsp
+	lea	rsi, [rsp+16]	; rsi = rem @ rsp+16
+	call	sys_nanosleep
+	cmp	rax, 0
+	jge	sleep_quit	; no error :)
+sleep_error:
+	neg	rax
+	cmp	rax, 4		; rax == EINTR?
+	jne	sleep_failed
+sleep_interrupted:
+	lea	rsi, [rsp+16]
+	mov	rax, [rsi]	; return rem.tv_sec
+	jmp	sleep_quit
+sleep_failed:
+	mov	rax, 0		; return 0 on error
+sleep_quit:
+	add	rsp, 32
+	ret
 
     global sigrestore:function
 sigrestore:
     mov rax, 15
     syscall
     ret
-
 
     global setjmp:function
 setjmp:
@@ -56,7 +134,6 @@ setjmp:
 
     mov rax, 0
     ret
-
 
     global longjmp:function
 longjmp:
--- proj3/src/libmini.h
+++ proj3/src/libmini.h
@@ -12,6 +12,54 @@
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
 
+/* from /usr/include/asm-generic/fcntl.h */
+#define O_ACCMODE	00000003
+#define O_RDONLY	00000000
+#define O_WRONLY	00000001
+#define O_RDWR		00000002
+#ifndef O_CREAT
+#define O_CREAT		00000100	/* not fcntl */
+#endif
+#ifndef O_EXCL
+#define O_EXCL		00000200	/* not fcntl */
+#endif
+#ifndef O_NOCTTY
+#define O_NOCTTY	00000400	/* not fcntl */
+#endif
+#ifndef O_TRUNC
+#define O_TRUNC		00001000	/* not fcntl */
+#endif
+#ifndef O_APPEND
+#define O_APPEND	00002000
+#endif
+#ifndef O_NONBLOCK
+#define O_NONBLOCK	00004000
+#endif
+#ifndef O_DSYNC
+#define O_DSYNC		00010000	/* used to be O_SYNC, see below */
+#endif
+#ifndef FASYNC
+#define FASYNC		00020000	/* fcntl, for BSD compatibility */
+#endif
+#ifndef O_DIRECT
+#define O_DIRECT	00040000	/* direct disk access hint */
+#endif
+#ifndef O_LARGEFILE
+#define O_LARGEFILE	00100000
+#endif
+#ifndef O_DIRECTORY
+#define O_DIRECTORY	00200000	/* must be a directory */
+#endif
+#ifndef O_NOFOLLOW
+#define O_NOFOLLOW	00400000	/* don't follow links */
+#endif
+#ifndef O_NOATIME
+#define O_NOATIME	01000000
+#endif
+#ifndef O_CLOEXEC
+#define O_CLOEXEC	02000000	/* set close_on_exec */
+#endif
+
 #define PERRMSG_MIN 0
 #define PERRMSG_MAX 34
 #define PERRMSG_UNKNWON "Unknown"
@@ -115,6 +163,12 @@
  *******************/
 typedef unsigned long long size_t;
 typedef long long ssize_t;
+typedef long long off_t;
+typedef int mode_t;
+typedef int uid_t;
+typedef int gid_t;
+typedef int pid_t;
+
 typedef unsigned long sigset_t;
 typedef void __signalfn_t(int);
 typedef __signalfn_t *__sighandler_t;
@@ -131,6 +185,16 @@ struct timespec {
     long    tv_nsec;    /* nanoseconds */
 };
 
+struct timeval {
+	long	tv_sec;		/* seconds */
+	long	tv_usec;	/* microseconds */
+};
+
+struct timezone {
+	int	tz_minuteswest;	/* minutes west of Greenwich */
+	int	tz_dsttime;	/* type of DST correction */
+};
+
 struct sigaction {
     __sighandler_t sa_handler;
     unsigned long sa_flags;
@@ -143,9 +207,6 @@ extern long errno;
 /************************
  * Function Declaration *
  ************************/
-ssize_t write(int fd, const void *buf, size_t count);
-long sys_write(unsigned int fd, const char *buf, size_t count);
-
 __sighandler_t signal(int signum, __sighandler_t handler);
 int sigaction(int sig, const struct sigaction *restrict act, struct sigaction *restrict oact);
 long sys_rt_sigaction(int sig, const struct sigaction *act, struct sigaction *oact, size_t sigsetsize);
@@ -166,25 +227,88 @@ void sigrestore();
 int sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
 long sys_rt_sigprocmask(int how, const sigset_t *nset, sigset_t *oset, size_t sigsetsize);
 
-int pause();
-long sys_pause();
-
-unsigned int sleep(unsigned int seconds);
-long sys_nanosleep(struct timespec *rqtp, struct timespec *rmtp);
-
 unsigned int alarm(unsigned int seconds);
 long sys_alarm(unsigned int seconds);
-
-void exit(int error_code);
-long sys_exit(int error_code) __attribute__ ((noreturn));
 
 int sigpending(sigset_t *set);
 long sys_rt_sigpending(sigset_t *set, size_t sigsetsize);
 
-void perror(const char *prefix);
-size_t strlen(const char *s);
-
 int setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int val);
+
+/* system calls */
+long sys_read(int fd, char *buf, size_t count);
+long sys_write(int fd, const void *buf, size_t count);
+long sys_open(const char *filename, int flags, ... /*mode*/);
+long sys_close(unsigned int fd);
+long sys_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off);
+long sys_mprotect(void *addr, size_t len, int prot);
+long sys_munmap(void *addr, size_t len);
+long sys_pipe(int *filedes);
+long sys_dup(int filedes);
+long sys_dup2(int oldfd, int newfd);
+long sys_pause();
+long sys_nanosleep(struct timespec *rqtp, struct timespec *rmtp);
+long sys_fork(void);
+long sys_exit(int error_code) __attribute__ ((noreturn));
+long sys_getcwd(char *buf, size_t size);
+long sys_chdir(const char *pathname);
+long sys_rename(const char *oldname, const char *newname);
+long sys_mkdir(const char *pathname, int mode);
+long sys_rmdir(const char *pathname);
+long sys_creat(const char *pathname, int mode);
+long sys_link(const char *oldname, const char *newname);
+long sys_unlink(const char *pathname);
+long sys_readlink(const char *path, char *buf, size_t bufsz);
+long sys_chmod(const char *filename, mode_t mode);
+long sys_chown(const char *filename, uid_t user, gid_t group);
+long sys_umask(int mask);
+long sys_gettimeofday(struct timeval *tv, struct timezone *tz);
+long sys_getuid();
+long sys_getgid();
+long sys_setuid(uid_t uid);
+long sys_setgid(gid_t gid);
+long sys_geteuid();
+long sys_getegid();
+
+/* wrappers */
+ssize_t	read(int fd, char *buf, size_t count);
+ssize_t	write(int fd, const void *buf, size_t count);
+int	open(const char *filename, int flags, ... /*mode*/);
+int	close(unsigned int fd);
+void *	mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off);
+int	mprotect(void *addr, size_t len, int prot);
+int	munmap(void *addr, size_t len);
+int	pipe(int *filedes);
+int	dup(int filedes);
+int	dup2(int oldfd, int newfd);
+int	pause();
+int	nanosleep(struct timespec *rqtp, struct timespec *rmtp);
+pid_t	fork(void);
+void	exit(int error_code);
+char *	getcwd(char *buf, size_t size);
+int	chdir(const char *pathname);
+int	rename(const char *oldname, const char *newname);
+int	mkdir(const char *pathname, int mode);
+int	rmdir(const char *pathname);
+int	creat(const char *pathname, int mode);
+int	link(const char *oldname, const char *newname);
+int	unlink(const char *pathname);
+ssize_t	readlink(const char *path, char *buf, size_t bufsz);
+int	chmod(const char *filename, mode_t mode);
+int	chown(const char *filename, uid_t user, gid_t group);
+int	umask(int mask);
+int	gettimeofday(struct timeval *tv, struct timezone *tz);
+uid_t	getuid();
+gid_t	getgid();
+int	setuid(uid_t uid);
+int	setgid(gid_t gid);
+uid_t	geteuid();
+gid_t	getegid();
+
+void bzero(void *s, size_t size);
+size_t strlen(const char *s);
+void perror(const char *prefix);
+unsigned int sleep(unsigned int s);
 
 #endif /* __LIBMINI_H__ */
